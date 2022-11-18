b8c09a453f313f010b6a1f42a833b9742671dcfd
Frédéric Laudarin <frederic.laudarin@365talents.com>
2022-11-18 17:37:13 +0100

    feat: percentiles on peak values

--- stats/peaks.py
+++ stats/peaks.py
@@ -1,3 +1,4 @@
+import matplotlib.pyplot as plt
 import numpy as np
 
 from time_series import TimeSeries
@@ -11,11 +12,37 @@ def find(time_series: TimeSeries):
 
 
 class Statistics:
+    @property
+    def plots(self) -> "Plots":
+        return self._plots
+
     def __init__(self, time_series: TimeSeries, indices: list[int]) -> None:
         self._time_series = time_series
         self._indices = indices
+        self._plots = Plots(self)
+        self._values = [self._time_series.resource[index] for index in self._indices]
 
     def datetimes(self, threshold=0.0):
         return [
             self._time_series.time[index] for index in self._indices if self._time_series.resource[index] > threshold
         ]
+
+    def values(self):
+        return self._values
+
+    def percentiles(self, *percents: float, method="linear"):
+        return np.percentile(self._values, percents, method=method)[0]
+
+
+class Plots:
+    def __init__(self, statistics: Statistics) -> None:
+        self._statistics = statistics
+
+    def histogram(self, bins=10, xlim=None):
+        fig, ax = plt.subplots()
+
+        ax.hist(self._statistics.values(), bins=bins, linewidth=0.5, edgecolor="white")
+        if xlim:
+            ax.set(xlim=xlim)
+
+        plt.show()
