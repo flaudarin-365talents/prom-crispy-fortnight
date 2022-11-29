9f302a36c30686392d37f8d699cc207ca6fc8341
Frédéric Laudarin <93928368+flaudarin-365talents@users.noreply.github.com>
2022-11-29 14:18:48 +0100

    Feat/memory stats (#2)
    * Memory extraction from Prometheus
    
    * feat: time series interpolation
    
    * chore: git filtering of notebook outputs at commit

--- stats/base.py
+++ stats/base.py
@@ -1,7 +1,7 @@
 import numpy as np
 
+Vector = list[float] | np.ndarray
 
-def percentiles(
-    sample: list[float] | np.ndarray, percents: list[float] | float, method: str = "linear"
-) -> list[float] | float:
+
+def percentiles(sample: Vector, percents: list[float] | float, method: str = "linear") -> list[float] | float:
     return np.percentile(sample, percents, method=method).tolist()
